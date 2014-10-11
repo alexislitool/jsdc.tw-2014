20a8ead42bf937229dd6b04d914e2e5d82d8e00c
James Halliday <mail@substack.net>
2014-10-11 14:49:17 +0800

    dictdb examples

---
+++ example/dictdb/import.js
@@ -0,0 +1,22 @@
+var parse = require('parse-dictd');
+var through = require('through2');
+var fs = require('fs');
+var gunzip = require('zlib').createGunzip;
+var minimist = require('minimist');
+
+var db = require('level')('/tmp/dict.db');
+var ddb = require('dictdb')(db);
+
+var argv = minimist(process.argv.slice(2));
+
+var dstream = fs.createReadStream(argv._[0]).pipe(gunzip());
+var istream = fs.createReadStream(argv._[1]);
+
+parse(dstream, istream).pipe(through.obj(function (row, enc, next) {
+    var a = { word: fix(row.from), lang: argv.from };
+    var b = { word: row.to.map(fix), lang: argv.to };
+    ddb.link(a, b);
+    next();
+}));
+
+function fix (w) { return w.replace(/^\w+\.\s*|;$/g,'') }
